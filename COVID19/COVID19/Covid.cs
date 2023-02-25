bd7ef4653e1c57300ab5d042f2d200dc45196ecd
Manant0rn <m_manantorn@hotmail.com>
2023-02-25 20:18:14 +0700

    first commit

---
+++ COVID19/COVID19/Covid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COVID19
+{
+    public class Covid
+    {
+        public long updated { get; set; }
+        public string country { get; set; }
+        public int cases { get; set; }
+        public int todayCases { get; set; }
+        public int deaths { get; set; }
+        public int todayDeaths { get; set; }
+        public int recovered { get; set; }
+        public int todayRecovered { get; set; }
+        public int active { get; set; }
+        public int critical { get; set; }
+        public int casesPerOneMillion { get; set; }
+        public int deathsPerOneMillion { get; set; }
+        public int tests { get; set; }
+        public int testsPerOneMillion { get; set; }
+        public int population { get; set; }
+        public string continent { get; set; }
+        public int oneCasePerPeople { get; set; }
+        public int oneDeathPerPeople { get; set; }
+        public int oneTestPerPeople { get; set; }
+        public double activePerOneMillion { get; set; }
+        public double recoveredPerOneMillion { get; set; }
+        public double criticalPerOneMillion { get; set; }
+        public int Confirmed { get; set; }
+        public int Recovered { get; set; }
+        public int Hospitalized { get; set; }
+        public int Deaths { get; set; }
+        public int NewConfirmed { get; set; }
+        public int NewRecovered { get; set; }
+        public int NewHospitalized { get; set; }
+        public int NewDeaths { get; set; }
+        public string UpdateDate { get; set; }
+        public string DevBy { get; set; }
+    }
+}
---
+++ COVID19/COVID19/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace COVID19
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetDataCovid19();
+        }
+
+        private async void GetDataCovid19()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var response = await client.GetStringAsync("https://static.easysunday.com/covid-19/getTodayCases.json?fbclid=IwAR0S3eSwPvoHQKpU9rROS6gulGXLA6y1k5Hyvx2RYt-NcDxQFh-nnydQASk");
+                var datacovid = JsonConvert.DeserializeObject<Covid>(response);
+                date.Text = $"อัปเดตข้อมูลล่าสุด {datacovid.UpdateDate}";
+                cases.Text = $"{string.Format("{0:n0}", datacovid.cases)} คน";
+                casestoday.Text = $"( + {string.Format("{0:n0}",datacovid.todayCases)} คน)";
+                recovered.Text = $"{string.Format("{0:n0}", datacovid.recovered)} คน";
+                recoveredtoday.Text = $"( + {string.Format("{0:n0}", datacovid.todayRecovered)} คน)";
+                hospitalized.Text = $"{string.Format("{0:n0}", datacovid.Hospitalized)} คน";
+                hospitalizedtoday.Text = $"( + {string.Format("{0:n0}", datacovid.NewHospitalized)} คน)";
+                death.Text = $"{string.Format("{0:n0}", datacovid.deaths)} คน";
+                deathtoday.Text = $"( + {string.Format("{0:n0}", datacovid.NewDeaths)} คน)";
+                devby.Text = datacovid.DevBy;
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
